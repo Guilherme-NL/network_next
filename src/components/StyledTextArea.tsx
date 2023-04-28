73015651f0dcdca58698f4c0b3035544602b8803
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-04-28 10:50:40 -0300

    feat: add styled text area component

---
+++ src/components/StyledTextArea.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTextArea = styled.textarea`
+  height: 32px;
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #777777;
+  border-radius: 8px;
+  margin: 8px 0 8px 0;
+  padding: 8px;
+`;
+
+interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea: React.FC<TextAreaProps> = (props) => {
+  return <StyledTextArea {...props} />;
+};
+
+export default TextArea;
